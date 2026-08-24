a4735d7cf77e4d74dcb9783f75107376de0fbae9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-24 09:33:10 -0300

    feat: criando mais checkbox na page modo customizado

--- src/components/CustomModeForm/CustomModeForm.jsx
+++ src/components/CustomModeForm/CustomModeForm.jsx
@@ -5,12 +5,16 @@ function CustomModeForm() {
     const [nome, setNome] = useState("");
     const [ativarIA, setAtivarIA] = useState(false);
     const [gerarPDF, setGerarPDF] = useState(false);
+    const [resumirComIA, setResumirComIA] = useState(false);
+    const [organizarPasta, setOrganizarPasta] = useState(false);
 
     function salvarModo() {
         const modo = {
             nome,
             ativarIA,
-            gerarPDF
+            gerarPDF,
+            resumirComIA,
+            organizarPasta,
         };
 
         localStorage.setItem("modoCustomizado", JSON.stringify(modo));
@@ -26,16 +30,21 @@ function CustomModeForm() {
             setNome(modo.nome);
             setAtivarIA(modo.ativarIA);
             setGerarPDF(modo.gerarPDF);
+            setResumirComIA(modo.resumirComIA);
+            setOrganizarPasta(modo.organizarPasta);
         };
 
     }, []);
 
     return (
         <section className="custom-mode-form">
-            <div className="custom-mode-form-div">
-                <h2>Crie seu próprio modo.</h2>
+            <h2>Crie seu próprio modo.</h2>
 
+            <div className="custom-mode-form-div">    
+                <h3>Nome do modo</h3>
                 <input type="text" placeholder="Nome do Modo" value={nome} onChange={(e) => setNome(e.target.value)} />
+
+                <h3>Funções Especializadas</h3>
                 <label>
                     <input type="checkbox" checked={ativarIA} onChange={(e) => setAtivarIA(e.target.checked)} />
                     Ativar IA
@@ -43,6 +52,14 @@ function CustomModeForm() {
                 <label>
                     <input type="checkbox" checked={gerarPDF} onChange={(e) => setGerarPDF(e.target.checked)} />
                     Gerar PDF
+                </label>
+                <label>
+                    <input type="checkbox" checked={resumirComIA} onChange={(e) => setResumirComIA(e.target.checked)} />
+                    Resumir com IA
+                </label>
+                <label>
+                    <input type="checkbox" checked={organizarPasta} onChange={(e) => setOrganizarPasta(e.target.checked)} />
+                    Organizar em pastas
                 </label>
                 <button onClick={salvarModo}>Criar modo</button>
             </div>
